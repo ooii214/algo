9dd94435a374e69f7ce8cfea887372ab750e13f1
“jung” <ooii214@naver.com>
2020-08-03 13:21:42 +0900

    ADD : plus_one

---
+++ 2020-08-03/66_Plus_One.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+let plusOne = function (digits) {
+  let i = digits.length - 1;
+  let val = 0;
+  let carry = 1;
+  while (i >= 0 && carry) {
+    val = digits[i] + carry;
+    carry = Math.floor(val / 10);
+    digits[i] = val % 10;
+    i--;
+  }
+  if (carry) digits.unshift(carry);
+  return digits;
+};
